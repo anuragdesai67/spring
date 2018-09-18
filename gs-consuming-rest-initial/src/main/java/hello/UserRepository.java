41652b683c5185f88da461a7887007b5f08ca549
anurag <adesai@iastate.edu>
2018-09-18 10:59:39 -0500

    initial

---
+++ gs-consuming-rest-initial/src/main/java/hello/UserRepository.java
@@ -0,0 +1,11 @@
+package hello;
+
+import org.springframework.data.repository.CrudRepository;
+
+import hello.User;
+
+import hello.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	
+}
